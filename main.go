5c0f99cb2ad2ea86bd9a92a01a8cdebb42aece8d
Илья Ильинов <ilinov.il@mail.ru>
2022-12-05 19:53:38 +0300

    "delete all"

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	cache := cache.New()
+
+	cache.Set("userId", 42)
+	userId := cache.Get("userId")
+
+	fmt.Println(userId)
+
+	cache.Delete("userId")
+	userId := cache.Get("userId")
+
+	fmt.Println(userId)
 }
