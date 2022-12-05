a9eaea137be97461f0bc781e20bf59bc65f91ad2
Илья Ильинов <ilinov.il@mail.ru>
2022-12-05 20:02:31 +0300

    rename cache

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	cache := cash.New()
+
+	cache.Set("userId", 42)
+	userId := cache.Get("userId")
+
+	fmt.Println(userId)
+
+	cache.Delete("userId")
+	userId := cache.Get("userId")
+
+	fmt.Println(userId)
 }
